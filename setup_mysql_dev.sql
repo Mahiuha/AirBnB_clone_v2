c628351055a6a7af9cb78242547c356fa780a895
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-22 05:10:57 +0300

    a script that prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,6 @@
--- prepares a MySQL server for the AirBnB clone
--- create a database
+-- prepares a MySQL server for the project
+-- create the DB, an user 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- create a user and grant privileges
-GRANT ALL ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
--- grant SELECT privileges on performance_schema
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
