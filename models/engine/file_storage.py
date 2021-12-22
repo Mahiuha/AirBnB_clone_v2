e1336982ba291528d8d8b231a2216d91b50cf7eb
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-22 23:45:26 +0300

    Updated FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,7 +28,7 @@ class FileStorage:
         """
         if cls:
             return {key: obj for (key, obj) in self.__objects.items()
-                    if isinstance(obj, type(cls))}
+                    if isinstance(obj, cls)}
         return self.__objects
 
     def new(self, obj):
